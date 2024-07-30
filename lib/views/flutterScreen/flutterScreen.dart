e7f5df1b67eaa0231e846bb378ddc51f0c354444
Muflih001 <muhammedmuflih05@gmail.com>
2024-07-30 15:15:55 +0530

    commit

--- lib/views/flutterScreen/flutterScreen.dart
+++ lib/views/flutterScreen/flutterScreen.dart
@@ -4,6 +4,7 @@ import 'package:quiz_app/utils/constants/colorConstatns.dart';
 
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:quiz_app/views/resultScreen/resultScreen.dart';
+import 'package:quiz_app/views/widgets/optionCard.dart';
 
 class FlutterScreen extends StatefulWidget {
   const FlutterScreen({super.key});
@@ -235,67 +236,5 @@ class _FlutterScreenState extends State<FlutterScreen> {
         );
       }
     });
-  }
-}
-
-class optionCard extends StatelessWidget {
-  const optionCard({
-    super.key,
-    required this.optionText,
-    required this.isSelected,
-    required this.onTap,
-    required this.isCorrect,
-  });
-  final String optionText;
-  final bool isSelected;
-  final VoidCallback onTap;
-  final bool isCorrect;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 15.0),
-      child: InkWell(
-        onTap: onTap,
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: isCorrect
-                  ? Colors.green
-                  : isSelected
-                      ? Colors.red
-                      : Colors.grey,
-              width: 2,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Container(
-                  width: 336,
-                  child: Text(
-                    optionText,
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: isSelected && isCorrect
-                            ? Colors.green
-                            : AppColors.textColor),
-                  ),
-                ),
-                Icon(
-                  isSelected
-                      ? Icons.radio_button_checked
-                      : Icons.radio_button_off,
-                  color: isSelected && isCorrect ? Colors.green : Colors.white,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
